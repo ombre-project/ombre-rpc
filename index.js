1de9aace493ea640c1d8818d420063819ec102f1
zooteld <zooteld@gmail.com>
2020-02-04 22:40:14 -0600

    Changes for ombre

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
 'use strict'
 
-module.exports.Arqmad = require('./lib/arqmad-rpc.js')
-module.exports.ArqmaService = require('./lib/service-rpc.js')
+module.exports.ombred = require('./lib/ombred-rpc.js')
+module.exports.ombreService = require('./lib/service-rpc.js')
 
 // These exports will be deprecated in a future version.
 // Continue to use them at your own risk.
 module.exports.Walletd = require('./lib/service-rpc.js')
 module.exports.Service = require('./lib/service-rpc.js')
-module.exports.Client = require('./lib/arqmad-rpc.js')
+module.exports.Client = require('./lib/ombred-rpc.js')
